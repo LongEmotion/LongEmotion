c631a79850d12cb848118b5971d7c45d42693cd5
LongEmotion <1045231771@qq.com>
2025-08-22 14:05:09 +0800

    Add files via upload

---
+++ LongEmotion/run_qwen.sh
@@ -0,0 +1,11 @@
+# TASKS-List
+# Conversations 
+# Emotion_Classification 
+# Emotion_Detection 
+# QA
+# Reports_Summary
+# Questionnaire
+export CUDA_VISIBLE_DEVICES=2
+bash evaluate_qwen.sh baseline Emotion_Detection
+
+
